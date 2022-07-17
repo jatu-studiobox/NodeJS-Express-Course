77b979a6376b8741ee1df47e6fe66eb6d1a2c1e2
jatu-studiobox <tj_yai@hotmail.com>
2022-07-18 04:45:01 +0700

    Add all files to project
    Add all files to project

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express"); // import express เข้ามาใช้งาน
+const chalk = require("chalk"); // import chalk เข้ามาใช้งาน
+const debug = require("debug")("app"); // import debug เข้ามาใช้งาน
+const morgan = require("morgan");   // import morgan เข้ามาใช้งาน
+const app = express();  // ประกาศ app ให้ใช้งาน express
+const port = 3000;  // ค่า port ในการใช้งาน web app
+
+app.use(morgan("combined"));    // เรียกใช้งาน morgan
+
+app.get("/", (req, res) => {    // รับ request 'get' ที่ root uri
+    res.send("Hello, Jatu Tung");   // คำสั่งตอบกลับ
+});
+
+app.listen(port, () => {    // สั่งให้ app รอ request ที่ port ที่กำหนดไว้
+    // console.log("Listening on port %d", port);
+    // เรียกใช้งาน Chalk ในการแสดงผลใน terminal ให้เป็นสี
+    // console.log("Listening on port : " + chalk.green(port));
+    // ใช้ debug แทน console.log
+    debug("Listening on port : " + chalk.green(port));
+});
