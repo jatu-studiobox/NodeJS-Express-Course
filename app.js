108e0df27ffc141de0e567edce02f9c6172df00a
jatu-studiobox <tj_yai@hotmail.com>
2022-07-19 00:15:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express"); // import express เข้ามาใช้งาน
-const chalk = require("chalk"); // import chalk เข้ามาใช้งาน
-const debug = require("debug")("app"); // import debug เข้ามาใช้งาน
-const morgan = require("morgan");   // import morgan เข้ามาใช้งาน
+// const chalk = require("chalk"); // import chalk เข้ามาใช้งาน
+// const debug = require("debug")("app"); // import debug เข้ามาใช้งาน
+// const morgan = require("morgan");   // import morgan เข้ามาใช้งาน
 const app = express();  // ประกาศ app ให้ใช้งาน express
 const port = 3000;  // ค่า port ในการใช้งาน web app
 
@@ -12,9 +12,9 @@ app.get("/", (req, res) => {    // รับ request 'get' ที่ root uri
 });
 
 app.listen(port, () => {    // สั่งให้ app รอ request ที่ port ที่กำหนดไว้
-    // console.log("Listening on port %d", port);
+    console.log("Listening on port %d", port);
     // เรียกใช้งาน Chalk ในการแสดงผลใน terminal ให้เป็นสี
     // console.log("Listening on port : " + chalk.green(port));
     // ใช้ debug แทน console.log
-    debug("Listening on port : " + chalk.green(port));
+    // debug("Listening on port : " + chalk.green(port));
 });
